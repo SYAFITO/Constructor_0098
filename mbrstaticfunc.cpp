0cd063fdc334398081f53851b5f28d250bac8bdb
SYAFITO <Lenovo@DESKTOP-4CQ6R6H>
2024-05-13 21:14:14 +0700

    masukkan int Mahasiswa

--- mbrstaticfunc.cpp
+++ mbrstaticfunc.cpp
@@ -3,5 +3,22 @@
 using namespace std;
 
 class Mahasiswa{
+    private:
+        static int nim;
     
-}
+    public:
+        int id;
+        string nama;
+
+        void setID();
+        void printAll();
+
+        static void setNim(int pNim) { nim = pNim; }
+        static int getNim() { return nim; }
+
+        Mahasiswa(string pnama):nama(pnama){
+            setID();
+        }
+};
+
+int Mahasiswa::nim = 100;
