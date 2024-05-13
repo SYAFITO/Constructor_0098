d67e04a55e7683702c8c3c87ba137310b542baad
SYAFITO <Lenovo@DESKTOP-4CQ6R6H>
2024-05-13 21:16:55 +0700

    Masukan int main

--- mbrstaticfunc.cpp
+++ mbrstaticfunc.cpp
@@ -32,3 +32,21 @@ void Mahasiswa::printAll(){
     cout << "Nama   = " << nama << endl;
     cout << endl;
 }
+
+int main(){
+    Mahasiswa mhs1("Lia Kurnia");
+    Mahasiswa mhs2("Asroni");
+
+    Mahasiswa::setNim(9); // mengakses nim melalui static member function "setnim"
+    Mahasiswa mhs3("Andi Kurniawan");
+    Mahasiswa mhs4("Joko Purbo");
+
+    mhs1.printAll();
+    mhs2.printAll();
+    mhs3.printAll();
+    mhs4.printAll();
+
+    cout << "akses di luar object = " << Mahasiswa::getNim() << endl; //mengakses nim
+
+    return 0;
+}
