cb862e45aa41d19f26a9955289824d4aae176862
SYAFITO <Lenovo@DESKTOP-4CQ6R6H>
2024-05-13 21:29:30 +0700

    masukkan int main

--- Construktor.cpp
+++ Construktor.cpp
@@ -31,3 +31,15 @@ Mahasiswa::Mahasiswa(int iNim, string iNama){
     nim = iNim;
     nama = iNama;
 }
+
+int main(){
+    Mahasiswa mhs1;
+    Mahasiswa mhs2(20);
+    Mahasiswa mhs3("Syafito");
+    Mahasiswa mhs4(30, "Denova");
+
+    mhs1.cetak();
+    mhs2.cetak();
+    mhs3.cetak();
+    mhs4.cetak();
+}
